60a9055d1bd2a25515dec009435fdb604685ca18
Meyanis95 <yanis.meziane@essec.edu>
2025-01-13 18:26:12 +0100

    feat: adding the decider

--- examples/folded_sha256.rs
+++ examples/folded_sha256.rs
@@ -1,4 +1,5 @@
 #![allow(non_snake_case)]
+use ark_groth16::Groth16;
 use folded_sha256::folded_sha256::main::H;
 use folded_sha256::folded_sha256::utils::sha256_msg_block_sequence;
 
@@ -6,15 +7,20 @@ use clap::{Arg, Command};
 use folded_sha256::folded_sha256::main::FoldedSha256FCircuit;
 use std::time::Instant;
 
-use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as Projective};
-use ark_grumpkin::{constraints::GVar as GVar2, Projective as Projective2};
+use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as G1};
+use ark_grumpkin::{constraints::GVar as GVar2, Projective as G2};
 
 use ark_std::rand;
-use folding_schemes::commitment::{kzg::KZG, pedersen::Pedersen};
-use folding_schemes::folding::nova::{Nova, PreprocessorParam};
-use folding_schemes::frontend::FCircuit;
-use folding_schemes::transcript::poseidon::poseidon_canonical_config;
-use folding_schemes::FoldingScheme;
+use folding_schemes::{
+    commitment::{kzg::KZG, pedersen::Pedersen},
+    folding::{
+        nova::{decider_eth::Decider as DeciderEth, Nova, PreprocessorParam},
+        traits::CommittedInstanceOps,
+    },
+    frontend::FCircuit,
+    transcript::poseidon::poseidon_canonical_config,
+    Decider, FoldingScheme,
+};
 
 fn main() {
     let cmd = Command::new("Nova-based SHA256 circuit proof generation and verification")
@@ -53,14 +59,25 @@ fn main() {
     let mut rng = rand::rngs::OsRng;
 
     type N = Nova<
-        Projective,
+        G1,
         GVar,
-        Projective2,
+        G2,
         GVar2,
         FoldedSha256FCircuit<Fr>,
         KZG<'static, Bn254>,
-        Pedersen<Projective2>,
+        Pedersen<G2>,
         false,
+    >;
+    pub type D = DeciderEth<
+        G1,
+        GVar,
+        G2,
+        GVar2,
+        FoldedSha256FCircuit<Fr>,
+        KZG<'static, Bn254>,
+        Pedersen<G2>,
+        Groth16<Bn254>,
+        N,
     >;
 
     let param_gen_timer = Instant::now();
@@ -70,9 +87,19 @@ fn main() {
 
     println!("Initialize FoldingScheme");
     let mut folding_scheme = N::init(&nova_params, F_circuit, initial_state.clone()).unwrap();
-
     let param_gen_time = param_gen_timer.elapsed();
     println!("PublicParams::setup, took {:?} ", param_gen_time);
+
+    // prepare the Decider prover & verifier params
+    println!("Generating the Decider Public Parameters");
+    let preprocess_timer = Instant::now();
+    let (decider_pp, decider_vp) =
+        D::preprocess(&mut rng, nova_params, folding_scheme.clone()).unwrap();
+    println!(
+        "Nova::Decider preprocess, took: {:?}",
+        preprocess_timer.elapsed()
+    );
+
     let input: Vec<u8> = vec![0u8; input_len]; // All the input bytes are zero
     let block_sequence = sha256_msg_block_sequence(input);
 
@@ -101,13 +128,31 @@ fn main() {
     );
 
     // verify the recursive SNARK
-    println!("Run the Nova's IVC verifier");
+    // println!("Run the Nova's IVC verifier");
+    // let start = Instant::now();
+    // let ivc_proof = folding_scheme.ivc_proof();
+    // N::verify(
+    //     nova_params.1, // Nova's verifier params
+    //     ivc_proof,
+    // )
+    // .unwrap();
+    // println!("RecursiveSNARK::verify took {:?}", start.elapsed());
+
+    println!("Run the Nova's Decider Prover");
     let start = Instant::now();
-    let ivc_proof = folding_scheme.ivc_proof();
-    N::verify(
-        nova_params.1, // Nova's verifier params
-        ivc_proof,
+    let proof = D::prove(rng, decider_pp, folding_scheme.clone()).unwrap();
+    println!("generated Decider proof: {:?}", start.elapsed());
+
+    let verified = D::verify(
+        decider_vp.clone(),
+        folding_scheme.i,
+        folding_scheme.z_0.clone(),
+        folding_scheme.z_i.clone(),
+        &folding_scheme.U_i.get_commitments(),
+        &folding_scheme.u_i.get_commitments(),
+        &proof,
     )
     .unwrap();
-    println!("RecursiveSNARK::verify took {:?}", start.elapsed());
+    assert!(verified);
+    println!("Decider proof verification: {}", verified);
 }
